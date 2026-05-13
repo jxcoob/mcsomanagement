00106b666cdafa2e99231be8561cdcf010a59d47
jxcoob <jacobavnesh432@gmail.com>
2026-05-13 18:38:03 -0400

    Update and rename removeuser.js to remove.js

--- prefix commands/remove.js
+++ prefix commands/remove.js
@@ -1,46 +1,39 @@
 /**
- * prefix/removeuser.js
- * Command: -removeuser @user
+ * prefix/remove.js
+ * Command: -remove @user
  * Removes a user from the current ticket channel.
  * Restricted to role: 1498131737623007374
  */
-
 'use strict';
-
 const config      = require('../config');
 const ticketStore = require('../utils/ticketStore');
-
 const TICKET_CATEGORY = '1498131739325890742';
 const COMMAND_ROLE_ID = '1498131737623007374';
-
 module.exports = {
-  name: 'removeuser',
+  name: 'remove',
   async execute(message) {
+    await message.delete().catch(() => {});
     if (!message.member.roles.cache.has(COMMAND_ROLE_ID)) {
-      return message.reply({ content: 'You do not have permission to use this command.' });
+      return message.channel.send({ content: 'You do not have permission to use this command.' });
     }
-
     const channel = message.channel;
     if (channel.parentId !== TICKET_CATEGORY) {
-      return message.reply({ content: 'This command can only be used inside a ticket channel.' });
+      return message.channel.send({ content: 'This command can only be used inside a ticket channel.' });
     }
-
     const target = message.mentions.members.first();
     if (!target) {
-      return message.reply({ content: 'Please mention a user. Usage: `-removeuser @user`' });
+      return message.channel.send({ content: 'Please mention a user. Usage: `-remove @user`' });
     }
-
     const ticket = ticketStore.getOrHydrate(channel, config);
     if (ticket && target.id === ticket.ownerId) {
-      return message.reply({ content: 'You cannot remove the ticket owner from their own ticket.' });
+      return message.channel.send({ content: 'You cannot remove the ticket owner from their own ticket.' });
     }
-
     try {
       await channel.permissionOverwrites.delete(target.id);
-      return message.reply({ content: `✅ ${target} has been removed from this ticket.` });
+      return message.channel.send({ content: `${target} has been removed from this ticket.` });
     } catch (err) {
-      console.error('[BCSO] Failed to remove user:', err);
-      return message.reply({ content: 'Failed to remove that user from the ticket.' });
+      console.error('[MCSO] Failed to remove user:', err);
+      return message.channel.send({ content: 'Failed to remove that user from the ticket.' });
     }
   },
 };
